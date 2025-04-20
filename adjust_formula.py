afeb9e14b0609b71894c5170187587ac172f9fa0
ted <zoogree@gmail.com>
2025-04-21 03:53:09 +0900

    update: refactor formula handling by adding AdjustFormula class; implement methods for fixing formulas and handling edge cases

---
+++ adjust_formula.py
@@ -0,0 +1,76 @@
+OPS = ["*", "/", "+", "-"]
+
+#! TODO: 05 + 2 등 입력시 5 + 2 등으로 수정하는 기능 필요 (calc.py 단에서 수정해야할지, 숫자버튼에서 비어있는 상태 혹은 연산자 뒤 0이 선입력인지 고려할 것. 예: 비어있는상태 or 연산자, 괄호 뒤 등에 0 입력후 다른숫자 입력시: 05-> 5로 변경 필요)
+
+
+#! 모든 멤버함수들은 생성자에서 초기화한 formula를 void함수처럼 수정하지만, str타입으로 formula를 리턴하기도 한다.
+class AdjustFormula:
+    def __init__(self, formula:str = ""):
+        self.formula:str = formula
+        
+    def fix_missing_one(self) -> str:
+        # replace "(*" to "(1*"
+        if "(*" in self.formula:
+            self.formula = self.formula.replace("(*", "(1*")
+        return self.formula
+
+    def insert_multipication_after_percent(self) -> str:
+        # Ensure "*" is automatically inserted after "%" when necessary
+        if "%" in self.formula:
+            result = []
+            for i, char in enumerate(self.formula):
+                result.append(char)
+                if char == "%" and i + 1 < len(self.formula) and self.formula[i + 1] not in OPS + [")"]:
+                    result.append("*")  # "%" 뒤에 "*" 추가
+            self.formula = "".join(result)
+        return self.formula
+    
+    def fix_missing_parentheses_multipication(self) -> str:
+        # Add "*" for implicit multiplication (keyboard input only) (e.g: 3(2+1) -> 3*(2+1),  (3+2)4 -> (3+2)*4)
+        i = 0
+        while i < len(self.formula) - 1:
+            # Insert "*" before "(" if preceded by a number
+            if self.formula[i].isdigit() and self.formula[i + 1] == "(":
+                self.formula = self.formula[:i + 1] + "*" + self.formula[i + 1:]
+                i += 1
+            # Insert "*" after ")" if followed by a number
+            elif self.formula[i] == ")" and self.formula[i + 1].isdigit():
+                self.formula = self.formula[:i + 1] + "*" + self.formula[i + 1:]
+                i += 1
+            i += 1
+        return self.formula
+    
+    def close_unmatched_parentheses(self) -> str:
+        # Automatically close any unclosed parentheses in the formula
+        openedParentheses = self.formula.count("(")
+        closedParentheses = self.formula.count(")")
+        if openedParentheses > closedParentheses:
+            for _ in range(openedParentheses-closedParentheses):
+                self.formula += ")"    
+        return self.formula
+
+    def handle_missing_formula(self) -> str:
+        # If the formula is empty, set the result to '0' by default
+        if self.formula == "": self.formula = '0'
+        return self.formula
+
+
+    def replace_percent_with_division(self) -> str:
+        # Replace "/ 100" instead of "%"
+        if "%" in self.formula:
+            for _ in range(self.formula.count("%")):
+                self.formula = self.formula.replace("%", "/100")  
+        return self.formula
+
+
+
+    def get_standard_fix(self) -> str:
+        self.fix_missing_one()
+        self.insert_multipication_after_percent()
+        self.fix_missing_parentheses_multipication()
+        self.close_unmatched_parentheses()
+        self.handle_missing_formula()
+        self.replace_percent_with_division()
+        return self.formula
+    
+    
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from calc import calc
+from adjust_formula import AdjustFormula
 
 OPS = ["*", "/", "+", "-"]
 BUTTON_FONT = "Courier", 25, "bold"
@@ -12,15 +13,15 @@ Last_Display = ""                  # 계산 기록 추가용 임시 변수: glob
 Prepare_for_New_input = False      # this trigger will turn on True after calc. (for clear display if you click number.)
 
 
-#### Inheritance functions
+#### Inheritance functions ####
 
-def check_safe_for_eval(user_input):  #Check unauthorized input because this code use eval(this is dangerous) function.
+def check_safe_for_eval(user_input):        #Check unauthorized input because this code use eval(this is dangerous) function.
     for char in user_input:
         if char not in ALLOWED_CHARS:
             return False 
     return True
 
-def find_last_ops_index(user_input):     # Used as an inheritance function
+def find_last_ops_index(user_input):        # Used as an inheritance function
     last_ops_index = 0
     chars = list("/-+*&()")
     replace = list(user_input)
@@ -31,14 +32,14 @@ def find_last_ops_index(user_input):     # Used as an inheritance function
                 replace[replace.index(c)] = "removed"
     return last_ops_index
 
-def update_input_ready_status(func=False):     #first_input trigger switching method. 
+def update_input_ready_status(func=False):  #first_input trigger switching method. 
     global Prepare_for_New_input   
     if func == True:
         Prepare_for_New_input = True
     else:
         Prepare_for_New_input = False 
         
-def pop_last_ops() -> None:     # remove the last operator if the last input is an operator. - Used as an inheritance function.
+def pop_last_invalid_ops() -> None:         # remove the last operator if the last input is an operator. - Used as an inheritance function.
     current = display_entry.get()
     if len(current) > 0:
         if current[-1] in OPS:
@@ -49,69 +50,16 @@ def invalid_formula_length(formula:str) -> bool:        # Check if the formula e
     if len(formula) > INPUT_LIMIT: return True
     return False
 
-
-#! pop_last_ops() 부분 내부구현 고려.-> 진행중.
-#! adjust_formula 기능이 더 첨가될 것을 고려하여 모듈화 고민(각기능 함수화).
-#! TODO: 05 + 2 등 입력시 5 + 2 등으로 수정하는 기능 필요 (calc.py 단에서 수정해야할지, 숫자버튼에서 비어있는 상태 혹은 연산자 뒤 0이 선입력인지 고려할 것. 예: 비어있는상태 or 연산자, 괄호 뒤 등에 0 입력후 다른숫자 입력시: 05-> 5로 변경 필요)
-def adjust_formula(formula: str) -> str:                # Fix formula. 여러 입력오류들을 보정.
-    # replace "(*" to "(1*"
-    if "(*" in formula:
-        formula = formula.replace("(*", "(1*")
-    
-    # Ensure "*" is automatically inserted after "%" when necessary
-    if "%" in formula:
-        result = []
-        for i, char in enumerate(formula):
-            result.append(char)
-            if char == "%" and i + 1 < len(formula) and formula[i + 1] not in OPS + [")"]:
-                result.append("*")  # "%" 뒤에 "*" 추가
-        formula = "".join(result)
-    
-    # Add "*" for implicit multiplication (keyboard input only) (e.g: 3(2+1) -> 3*(2+1),  (3+2)4 -> (3+2)*4)
-    i = 0
-    while i < len(formula) - 1:
-        # Insert "*" before "(" if preceded by a number
-        if formula[i].isdigit() and formula[i + 1] == "(":
-            formula = formula[:i + 1] + "*" + formula[i + 1:]
-            i += 1
-        # Insert "*" after ")" if followed by a number
-        elif formula[i] == ")" and formula[i + 1].isdigit():
-            formula = formula[:i + 1] + "*" + formula[i + 1:]
-            i += 1
-        i += 1
-    i = 0       # Reset index for reprocessing after modifications
-    
-    # Automatically close any unclosed parentheses in the formula
-    openedParentheses = formula.count("(")
-    closedParentheses = formula.count(")")
-    if openedParentheses > closedParentheses:
-        for _ in range(openedParentheses-closedParentheses):
-            formula += ")"    
-
-    # If the formula is empty, set the result to '0' by default
-    if formula == "": formula = '0'
-
-    return formula
-
-#! TODO: error_print 조정중: 25-04-20
-def error_print(msg: str = "ERROR"):
-    """엔트리에 msg를 출력하고, 버튼을 비활성화 후 3초 후 복원"""
-    
-    def disable_button():
-        equals_button.config(bg="red", state=DISABLED)
-    
-    def restore_button():
-        equals_button.config(bg=COLORS[1], fg=COLORS[3], state=NORMAL)
-    
-    def clear_display():
-        display_entry.delete(0, END)
+def error_print(msg: str = "ERROR") -> None:    # Display a message in the entry, disable the button, and restore it after 3 seconds.    
+    def disable_button(): equals_button.config(bg="red", state=DISABLED)
+    def restore_button(): equals_button.config(bg=COLORS[1], fg=COLORS[3], state=NORMAL)
+    def clear_display(): display_entry.delete(0, END)
     
     disable_button()
     clear_display()
     display_entry.insert(0, msg)
     
-    # 3초 후 restore_button과 clear_display를 동시에 실행
-    window.after(3000, lambda: (restore_button(), clear_display()))
+    window.after(3000, lambda: (restore_button(), clear_display())) # windows에 전달할 함수가 복수라면 램다를 사용
 
 #! TEST: 정밀도 보정함수추가 : 25-04-20
 def adjust_precision(result: float) -> float:    
@@ -123,9 +71,9 @@ def adjust_precision(result: float) -> float:
     return result                
                 
                 
-#### Button functions.
+#### Button functions ####
 
-## Number (0~9) and Operators (*, /, +, -, %)  
+## Number (0~9) and Operators (*, /, +, -, %) ##
 def insert_multiplication():   # Auto insert "*" after ")" - Used as an inheritance function
     current = display_entry.get()
     if current != "":
@@ -140,25 +88,25 @@ def number_input(num):
     display_entry.insert(END, num)
     
 def operator_button(operator):
-    pop_last_ops()
+    pop_last_invalid_ops()
     update_input_ready_status()
     display_entry.insert(END, operator)
     
     
-## Function keys    (., C, +=, (), =)
-def point():
+## Function keys    (., C, +-, (), =) ##
+def point():    # '.' Button
     update_input_ready_status()
     display_entry.insert(END, ".")
-def clear():
+def clear():    # 'C' Button
     update_input_ready_status()
     display_entry.delete(0, END)
 
 # signchange() function turns the input into negative or positive based on various conditions. 
 # The content isn't really important. I just kept debugging until it worked as wished.    
-def signchange():
+def signchange():       # '+-' Button.
     update_input_ready_status()
     current = display_entry.get()
-    if current == "" or current == '0': return      #! DEBUG: clicked with empty or 0   (25-04-20)
+    if current == "" or current == '0': return      # DEBUG: clicked with empty or 0   (25-04-20)
     last_index = find_last_ops_index(current)    
     
     if len(current) > 0 and len(current)-1 != last_index and last_index > 0:      
@@ -189,7 +137,7 @@ def signchange():
             display_entry.delete(0)      
 
 # Auto Open/Closing: Algorithm for deciding whether to open or close parentheses when "pressing the corresponding button"            
-def parentheses():
+def parentheses():      # '( )' Bottn.
     update_input_ready_status()
     current = display_entry.get()
     left = current.count("(")
@@ -211,61 +159,57 @@ def parentheses():
         display_entry.insert(END, "error")
 
 #! TODO : equals() 함수내부 복잡도때문에 함수화 리팩토링 진행중
-def equals():       # Button Function.
+def equals():       # '=' Button.
     
-    #! TODO: adjust_formula() 에 내부구현 가능한지 리펙토링 검토.  : 현재 pop_last_ops()는 다른데서도 쓰이니 신중히 검토.
-    pop_last_ops()   # remove incorrect operators before calculation. ('1+3-=' > '1+3=')
+    #! TODO : str 리턴식으로 바꿀 것을 검토.
+    pop_last_invalid_ops()   # remove incorrect operators before calculation. ('1+3-=' > '1+3=')
         
-    current:str = adjust_formula(display_entry.get())       # 계산식 여러 오류 검토/보정
+    #! TEST: adjust_formula class화 (다양한 수식 오류 보정)
+    user_formula:str = AdjustFormula(display_entry.get()).get_standard_fix()
     
-    # check formula is valid length
-    if invalid_formula_length(current):        
-        window.after(0, lambda: error_print(f"OUT OF RANGE({len(current)}/{INPUT_LIMIT})"))
+    # 예외처리: check formula is valid length
+    if invalid_formula_length(user_formula):        
+        window.after(0, lambda: error_print(f"OUT OF RANGE({len(user_formula)}/{INPUT_LIMIT})"))
         return
-              
-    #! TODO: adjust_formula() 에 내부구현 가능한지 리펙토링 검토. : 어려운부분: 히스토리에는 last_questestion 사용, 내부적으로는 current사용.
-    # Replace "/ 100" instead of "%"
-    #!  /100 으로 유지하고 last_questestion 을 사용하지 않는방향이면 쉽게 가능. (아래 recent result 업데이트도 리팩토링 해야함.)
-    last_questestion = current  # maintain "%" on recent label
-    if "%" in current:
-        for _ in range(current.count("%")):
-            current = current.replace("%", "/100")
-            
+                  
     # Try make Result
+    user_formula_result:float = 0.0            # init user_formula_result.
+    
+    #! IMPORTANT TODO: 에러처리 검토. 잘못된 입력은 check_safe_for_eval에서 실패해야되는데 왜 except로 넘어가는지 모르겠음
     try:
-        if check_safe_for_eval(current):  #! 원래 eval검사용이었으나, calc로 바뀌고 현재 식에 사용된 문자 유효성 검사로 사용중.
-            result: float = calc(current)  # eval에서 calc모듈(직접작성) 으로 변경됨.
-            result: float = adjust_precision(result)   # 정밀도 보정 함수 적용 (25-04-20)
+        if check_safe_for_eval(user_formula):  #! 원래 eval검사용이었으나, calc로 바뀌고 현재 식에 사용된 문자 유효성 검사로 사용중.
+            user_formula_result = calc(user_formula)  # eval에서 calc모듈(직접작성) 으로 변경됨.
+            user_formula_result = adjust_precision(user_formula_result)   # 정밀도 보정 함수 적용 (25-04-20)
             update_input_ready_status(True)  # This trigger will clear display if you click number after calc.
         else:
-            raise ValueError("Unauthorized input")
+            raise ValueError("Unauthorized input")      #! TODO: raise 대신 except처럼 사용 고려.
         
-    # Exception Handling -> result = 'err'
+    # Exception Handling -> quit equals.
     except:
-        window.after(0, lambda: error_print("ERROR"))   
-        return      #! quit equal() instead of make result.
-        # result = "err"  
+        print(f"DEBUG: user_formula = {user_formula}, user_formula_result = {user_formula_result}")       #! 디버그출력
+        window.after(0, lambda: error_print("ERROR"))
+        return
         
-    # Update result on display_entry
+    # Update result on display_entry : entry의 계산식을 제거하고, 결과로 업데이트.
     display_entry.delete(0, END)
-    display_entry.insert(0, str(result))
+    display_entry.insert(0, str(user_formula_result))
     
     
-    #! TODO: 최근검사식 업데이트 함수화 고려.
+    #! TODO: 최근 계산결과 업데이트 기록기능:  함수화 고려.
     # Recent result labels Update (except input just "=")
     global Last_Display 
     if "=" in str(Last_Display):    # if Last_Display has result without fomula.
         Last_Display_result = Last_Display.split('=')[-1]   
     else:
         Last_Display_result = Last_Display  
+        
     Last_Display_2 = Last_Display
-    if result == "err":
-        Last_Display = (f"{last_questestion}={result}")  #for update recent label.
-    else:
-        Last_Display = (f"{last_questestion}={result:,}")  #for update recent label.
+    Last_Display = (f"{user_formula}={user_formula_result:,}")  #for update recent label.   #! last_questestion->current. 기능 삭제(%->/100) 25-04-21
+        
     if len(Last_Display) >= 40:
-        Last_Display = f"{result:,}"   #Last_Display is just result if that is longer than 27 chars
-    if Last_Display_result != current:  
+        Last_Display = f"{user_formula_result:,}"   #Last_Display is just result if that is longer than 27 chars
+        
+    if Last_Display_result != user_formula:  
         if recent_label_1["text"] == "":
             recent_label_1.config(text=Last_Display)
         else:
@@ -273,14 +217,14 @@ def equals():       # Button Function.
             recent_label_1.config(text=Last_Display)
             
             
-#### UI
-#Make Window
+#### UI ####
+## Make Window ##
 window = Tk()
 window.config(padx=15, pady=15, bg=COLORS[0], highlightthickness=0)
 window.title("Python calc")
 
-####DISPLAY FRAME####
-#Recent result label
+#### DISPLAY FRAME ####
+## Recent result label ##
 display_frame = Frame(window)
 display_frame.config(bg=COLORS[0], pady=15, highlightthickness=0)
 display_frame.grid(column=0, row=0)
@@ -288,15 +232,15 @@ recent_label_2 = Label(display_frame, text="", bg=COLORS[0], fg=COLORS[3])
 recent_label_2.grid(column=0, row=1, sticky='e')
 recent_label_1 = Label(display_frame, text="", bg=COLORS[0], fg=COLORS[3])
 recent_label_1.grid(column=0, row=2, sticky="e")
-#Display Entry
+## Display Entry ##
 display_entry = Entry(display_frame, text="Display", justify=RIGHT, width=24, highlightthickness=0, font=DISPLAY_FONT, bg=COLORS[2], fg=COLORS[0])
 # display_entry.focus()
 display_entry.grid(column=0, row=3)
 
-####BUTTONS FRAME ####
+#### BUTTONS FRAME ####
 button_frame = Frame(window)
 button_frame.grid(column=0,row=2)
-#FirstLine(Top)         #Tip. Use lambda to delay function call until event (e.g., button press)
+## FirstLine(Top)         #Tip. Use lambda to delay function call until event (e.g., button press)
 clear_button = Button(button_frame, text="C", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=clear, bg=COLORS[1], fg=COLORS[3])
 clear_button.grid(column=0, row=0)
 parentheses_button = Button(button_frame, text="( )", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=parentheses, bg=COLORS[2], fg=COLORS[3])
@@ -305,7 +249,7 @@ percent_button = Button(button_frame, text="%", font=BUTTON_FONT, width=3,height
 percent_button.grid(column=2, row=0)
 division_button = Button(button_frame, text="/", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("/"), bg=COLORS[2], fg=COLORS[3])
 division_button.grid(column=3, row=0)
-#SecondLine
+## SecondLine
 seven_button = Button(button_frame, text="7", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("7"), bg=COLORS[3], fg=COLORS[0])
 seven_button.grid(column=0, row=1)
 eight_button = Button(button_frame, text="8", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("8"), bg=COLORS[3], fg=COLORS[0])
@@ -314,7 +258,7 @@ nine_button = Button(button_frame, text="9", font=BUTTON_FONT, width=3,height=1,
 nine_button.grid(column=2, row=1)
 multiplication_button = Button(button_frame, text="*", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("*"), bg=COLORS[2], fg=COLORS[3])
 multiplication_button.grid(column=3, row=1)
-#ThirdLine
+## ThirdLine
 four_button = Button(button_frame, text="4", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("4"), bg=COLORS[3], fg=COLORS[0])
 four_button.grid(column=0, row=2)
 five_button = Button(button_frame, text="5", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("5"), bg=COLORS[3], fg=COLORS[0])
@@ -323,7 +267,7 @@ six_button = Button(button_frame, text="6", font=BUTTON_FONT, width=3,height=1, 
 six_button.grid(column=2, row=2)
 subtraction_button = Button(button_frame, text="-", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("-"), bg=COLORS[2], fg=COLORS[3])
 subtraction_button.grid(column=3, row=2)
-#FourthLine
+## FourthLine
 one_button = Button(button_frame, text="1", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("1"), bg=COLORS[3], fg=COLORS[0])
 one_button.grid(column=0, row=3)
 two_button = Button(button_frame, text="2", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("2"), bg=COLORS[3], fg=COLORS[0])
@@ -332,7 +276,7 @@ three_button = Button(button_frame, text="3", font=BUTTON_FONT, width=3,height=1
 three_button.grid(column=2, row=3)
 addition_button = Button(button_frame, text="+", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("+"), bg=COLORS[2], fg=COLORS[3])
 addition_button.grid(column=3, row=3)
-#FifthLine(Bottom)
+## FifthLine(Bottom)
 signchange_button = Button(button_frame, text="+/-", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=signchange, bg=COLORS[3], fg=COLORS[0])
 signchange_button.grid(column=0, row=4)
 zero_button = Button(button_frame, text="0", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("0"), bg=COLORS[3], fg=COLORS[0])
@@ -342,7 +286,7 @@ point_button.grid(column=2, row=4)
 equals_button = Button(button_frame, text="=", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=equals, bg=COLORS[1], fg=COLORS[3])
 equals_button.grid(column=3, row=4)
 
-#### Bind keyboard  - Button input is preferred. Only some function keys are bound to the keyboard.
+#### Bind keyboard  - Button input is preferred. Only some function keys are bound to the keyboard. ####
 def backspace(event):    # It's created to handle the scenario where the entry widget loses focus.
     current = display_entry.get()
     focus = str(window.focus_get())        #TIP. tkinter focus_get() return your active widget name.
@@ -354,6 +298,5 @@ def backspace(event):    # It's created to handle the scenario where the entry w
 window.bind("<BackSpace>", backspace) 
 window.bind("<Escape>", lambda event: clear())      #TIP. bind need 'event' argument. use lambda because it is very simple. 
 window.bind("<Return>", lambda event: equals())
-
 
 window.mainloop()
